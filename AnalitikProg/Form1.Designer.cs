5a591b75e36c985d289a1fcfc5ab172b6dbdcc00
Alex18099 <sasharodionow@gmail.com>
2023-06-20 17:54:36 +0300

    Добавьте файлы проекта.

---
+++ AnalitikProg/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AnalitikProg
+{
+    partial class AnalitikProg
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnalitikProg));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonGenerate = new System.Windows.Forms.ToolStripButton();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonGenerate});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonGenerate
+            // 
+            this.toolStripButtonGenerate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonGenerate.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonGenerate.Image")));
+            this.toolStripButtonGenerate.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonGenerate.Name = "toolStripButtonGenerate";
+            this.toolStripButtonGenerate.Size = new System.Drawing.Size(138, 22);
+            this.toolStripButtonGenerate.Text = "Сформировать график";
+            this.toolStripButtonGenerate.Click += new System.EventHandler(this.toolStripButtonGenerate_Click);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 25);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(800, 425);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // AnalitikProg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "AnalitikProg";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonGenerate;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+    }
+}
+
---
+++ AnalitikProg/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Markup;
+using LiveCharts.Wpf;
+using LiveCharts;
+
+namespace AnalitikProg
+{
+    public partial class AnalitikProg : Form
+    {
+        List<Data> datas;
+        public AnalitikProg()
+        {
+            InitializeComponent();
+            datas = new List<Data>()
+            {
+                new Data(){ values=35000,date=new DateTime(2020,01,01) },
+                new Data(){ values=30000,date=new DateTime(2020,01,02) },
+                new Data(){ values=40000,date=new DateTime(2020,01,03) },
+                new Data(){ values=55000,date=new DateTime(2020,01,04) },
+                new Data(){ values=55000,date=new DateTime(2020,01,05) },
+                new Data(){ values=30000,date=new DateTime(2020,01,06) },
+                new Data(){ values=35000,date=new DateTime(2020,01,07) },
+                new Data(){ values=42000,date=new DateTime(2020,01,08) },
+                new Data(){ values=57000,date=new DateTime(2020,01,09) },
+                new Data(){ values=88000,date=new DateTime(2020,01,10) },
+                new Data(){ values=51000,date=new DateTime(2020,01,11) },
+                new Data(){ values=65000,date=new DateTime(2020,01,12) },
+                new Data(){ values=100000,date=new DateTime(2020,01,13) },
+                new Data(){ values=75000,date=new DateTime(2020,01,14) },
+                new Data(){ values=88000,date=new DateTime(2020,01,15) },
+                new Data(){ values=92000,date=new DateTime(2020,01,16) },
+                new Data(){ values=99000,date=new DateTime(2020,01,17) },
+                new Data(){ values=87000,date=new DateTime(2020,01,18) },
+                new Data(){ values=110000,date=new DateTime(2020,01,19) }
+            };
+        }
+
+        private void toolStripButtonGenerate_Click(object sender, EventArgs e)
+        {
+            SeriesCollection series = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<int> zp = new ChartValues<int>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<string> date = new List<string>(); //здесь будут храниться значения для оси X
+            foreach (var item in datas) //Заполняем коллекции
+            {
+                zp.Add(item.values);
+                date.Add(item.date.ToShortDateString());
+            }
+
+            cartesianChart1.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart1.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "Дата",
+                Labels = date
+            });
+
+            LineSeries line = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line.Title = "График";
+            line.Values = zp;
+
+            series.Add(line); //Добавляем линию на график
+            cartesianChart1.Series = series; //Отрисовываем график в интерфейсе
+        }
+        class Data
+        {
+            public int values { get; set; }
+            public DateTime date { get; set; }
+        }
+    }
+}
